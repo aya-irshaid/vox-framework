c6f29ec04145b38a6b3d515ed9bf5b7ed35d2285
Aya Irshaid <aya.irshaid-e@maf.ae>
2026-03-24 11:58:46 +0300

    Update Package.swift
    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Vox",
-            url: "https://github.com/aya-irshaid/vox-framework.git/Vox.xcframework.zip",
+            url: "https://github.com/aya-irshaid/vox-framework/releases/download/0.1.242/Vox.xcframework.zip",
             checksum: "5b74c5b8b5b23b28e8ad0b93f4677f25841916d87bb9a42c05b07b73bca07200"
         )
     ]
